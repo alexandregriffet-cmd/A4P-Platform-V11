ca265c165a889cf7cee850eb6c09757123a5944d
alexandregriffet-cmd <alexandre.griffet@gmail.com>
2026-03-15 00:19:23 +0100

    Add files via upload

---
+++ lib/constants.ts
@@ -0,0 +1,4 @@
+export const HUB_URL = "https://alexandregriffet-cmd.github.io/A4P-Diagnostic-Academie-de-Performances-/"
+export const CMP_URL = "https://alexandregriffet-cmd.github.io/CMP-A4P-ACADEMIE-DE-PERFORMANCES-/"
+export const PMP_URL = "https://alexandregriffet-cmd.github.io/PMP-A4P-Acad-mie-de-Performances-/"
+export const PSYCHO_URL = "https://alexandregriffet-cmd.github.io/Module-psycho-motionnelle/"
