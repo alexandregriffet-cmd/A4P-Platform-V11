91e3415a45a10466426652420b777b1230c2e129
alexandregriffet-cmd <alexandre.griffet@gmail.com>
2026-03-17 23:58:01 +0100

    Update route.ts

--- app/api/send_club_report/route.ts
+++ app/api/send_club_report/route.ts
@@ -1,1 +1,19 @@
+import { NextResponse } from 'next/server'
 
+export async function POST(req: Request) {
+  try {
+    const body = await req.json()
+
+    console.log('Email data:', body)
+
+    return NextResponse.json({
+      success: true,
+      message: 'Email simulation OK',
+    })
+  } catch (error) {
+    return NextResponse.json(
+      { success: false, error: 'Erreur API' },
+      { status: 500 }
+    )
+  }
+}
